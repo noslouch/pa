e52c21daa48a2dd67dfe5e25238b7902c944ab79
Brian Whitton <me@brianwhitton.com>
2013-08-05 10:01:02 -0400

    configured EE database

---
+++ system/expressionengine/config/database.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'expressionengine';
+$active_record = TRUE;
+
+$db['expressionengine']['hostname'] = 'us-cdbr-east-04.cleardb.com';
+$db['expressionengine']['username'] = 'b86903d8fe453c';
+$db['expressionengine']['password'] = '4376d9be';
+$db['expressionengine']['database'] = 'heroku_f0ff3901de97924';
+$db['expressionengine']['dbdriver'] = 'mysql';
+$db['expressionengine']['pconnect'] = FALSE;
+$db['expressionengine']['dbprefix'] = 'exp_';
+$db['expressionengine']['swap_pre'] = 'exp_';
+$db['expressionengine']['db_debug'] = TRUE;
+$db['expressionengine']['cache_on'] = FALSE;
+$db['expressionengine']['autoinit'] = FALSE;
+$db['expressionengine']['char_set'] = 'utf8';
+$db['expressionengine']['dbcollat'] = 'utf8_general_ci';
+$db['expressionengine']['cachedir'] = '/Library/WebServer/Documents/pa/system/expressionengine/cache/db_cache/';
+
+/* End of file database.php */
+/* Location: ./system/expressionengine/config/database.php */
