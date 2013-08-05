55ae0e18f2b116046caad2f159f80bf27ef3c66f
Brian Whitton <me@brianwhitton.com>
2013-08-05 12:02:06 -0400

    ignoring php templates for the moment

--- php_files/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $_GET['template'] = 'home';
-    include("templates/includes/header.php"); 
-?>
-        <h2 class="visuallyhidden">Home</h2>
-        <?php include("templates/includes/quotes.php"); ?>
-
-<?php include("templates/includes/footer.php"); ?>
