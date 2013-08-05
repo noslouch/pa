c841dd785f142845f50183a70fdff14d9cb59d20
Brian Whitton <me@brianwhitton.com>
2013-08-05 09:42:07 -0400

    inflated ee

---
+++ templates-php/index.php
@@ -0,0 +1,8 @@
+<?php 
+    $_GET['template'] = 'home';
+    include("templates/includes/header.php"); 
+?>
+        <h2 class="visuallyhidden">Home</h2>
+        <?php include("templates/includes/quotes.php"); ?>
+
+<?php include("templates/includes/footer.php"); ?>
