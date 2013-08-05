e05e4aff50fa6d5760e8b3774dfdf9019da2d875
Brian Whitton <me@brianwhitton.com>
2013-08-05 09:06:40 -0400

    test commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $_GET['template'] = 'home';
     include("templates/includes/header.php"); 
 ?>
-        <h2 class="visuallyhidden">Home</h2>
+        <h2 class="visuallyhidden">Home*</h2>
         <?php include("templates/includes/quotes.php"); ?>
 
 <?php include("templates/includes/footer.php"); ?>
