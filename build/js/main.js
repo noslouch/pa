cd9e671a4d16b042900ba402a39c5c8becd83dc9
Brian Whitton <me@brianwhitton.com>
2013-08-23 16:28:55 -0400

    cleaned directories. removed fancybox spinner

--- build/js/main.js
+++ build/js/main.js
@@ -11,11 +11,6 @@ $('#n-container header').click(function(e){
     $(qContainer).toggleClass('short')
 })
 
-$(document).on('click', '#starfield a', function(e){
-    //e.preventDefault()
-    //e.stopPropagation()
-})
-
 var filterBar = document.getElementById('filter-bar')
 var $filters = $('.filter a')
 
