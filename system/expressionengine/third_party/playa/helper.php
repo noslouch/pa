2b58c230ce1c7ae796fb7eaa8c27b43177950bb8
Brian Whitton <me@brianwhitton.com>
2013-08-26 07:23:24 -0400

    updated playa

--- system/expressionengine/third_party/playa/helper.php
+++ system/expressionengine/third_party/playa/helper.php
@@ -6,6 +6,8 @@
 */
 class Playa_Helper {
 
+	private static $_order_by = "";
+
 	/**
 	 * Constructor
 	 */
@@ -30,33 +32,35 @@ class Playa_Helper {
 	/**
 	 * Parameter => SQL
 	 */
-	function param2sql($param, &$not = NULL, $use_not = TRUE)
+	function param2sql($param)
 	{
+		$not = FALSE;
+
 		if (is_string($param))
 		{
-			if (strlen($param) > 4 && strtolower(substr($param, 0, 4)) == 'not ')
+			if (strlen($param) > 4 && (strtolower(substr($param, 0, 4)) == 'not ') || (strtolower(substr($param, 0, 4)) == 'not_'))
 			{
 				$not = TRUE;
 				$param = substr($param, 4);
 			}
-			else
+
+			if (strtolower($param) == 'current_user')
 			{
-				$not = FALSE;
+				$param = $this->EE->session->userdata('member_id');
 			}
 
 			$param = explode('|', $param);
 		}
-		else if ($not === NULL)
+		if (is_integer($param))
 		{
-			$not = FALSE;
+			$param = array($param);
 		}
-
 		if (count($param) == 1)
 		{
-			return ($not && $use_not ? '<>' : '=').' "'.$param[0].'"';
+			return ($not ? '<>' : '=').' "'.$param[0].'"';
 		}
 
-		return ($not && $use_not ? 'NOT ' : '').'IN ("'.implode('","', $param).'")';
+		return ($not ? 'NOT ' : '').'IN ("'.implode('","', $param).'")';
 	}
 
 	/**
@@ -102,17 +106,18 @@ class Playa_Helper {
 	/**
 	 * Entries Query
 	 */
-	function entries_query($params, $add_to_sql = array())
+	function entries_query($params)
 	{
 		// -------------------------------------------
 		//  Param name mapping
 		// -------------------------------------------
 
 		$param_mapping = array(
-			'author'      => 'author_id',
-			'category_id' => 'category',
-			'weblog'      => 'channel',
-			'weblog_id'   => 'channel_id'
+			'author'        => 'author_id',
+			'category_id'   => 'category',
+			'weblog'        => 'channel',
+			'weblog_id'     => 'channel_id',
+			'member_groups' => 'group_id'
 		);
 
 		foreach ($param_mapping as $old_name => $new_name)
@@ -128,9 +133,20 @@ class Playa_Helper {
 		//  Prepare the SQL
 		// -------------------------------------------
 
-		$sql = 'SELECT '.(isset($params['count']) ? 'COUNT(ct.entry_id) count' : 'ct.*')
-		     . (isset($add_to_sql['select']) ? ', '.$add_to_sql['select'] : '')
-		     . ' FROM exp_channel_titles ct'.(isset($add_to_sql['from']) ? ', '.$add_to_sql['from'] : '');
+		if (!empty($params['count']))
+		{
+			$sql_select = 'SELECT COUNT(ct.entry_id) count';
+		}
+		else
+		{
+			$sql_select = 'SELECT ct.*';
+		}
+
+		$sql_from = "\nFROM exp_channel_titles ct";
+
+		$join_members = FALSE;
+		$join_channels = FALSE;
+		$join_cat_posts = FALSE;
 
 		$where = array();
 
@@ -138,93 +154,51 @@ class Playa_Helper {
 			//  Author
 			// -------------------------------------------
 
-			if (isset($params['author_id']) && $params['author_id'])
+			if (!empty($params['author_id']))
 			{
-				$where[] = 'ct.author_id '.$this->param2sql($params['author_id']);
+				$where[] = 'ct.author_id '.$this->param2sql($this->_escape_variable($params['author_id']));
 			}
 
 			// -------------------------------------------
 			//  Author Group
 			// -------------------------------------------
 
-			if (isset($params['group_id']) && $params['group_id'])
+			if (!empty($params['group_id']))
 			{
-				// get filtered list of author ids
-				$not = NULL;
-				$query = $this->EE->db->query('SELECT member_id FROM exp_members
-				                               WHERE group_id '.$this->param2sql($params['group_id'], $not, FALSE));
-
-				if (! $query->num_rows())
-				{
-					if (! $not) return FALSE;
-				}
-				else
-				{
-					$author_ids = array();
-					foreach ($query->result_array() as $row)
-					{
-						$author_ids[] = $row['member_id'];
-					}
-
-					$where[] = 'ct.author_id '.$this->param2sql($author_ids, $not);
-				}
+				$join_members = TRUE;
+				$where[] = 'm.group_id '.$this->param2sql($this->_escape_variable($params['group_id']));
 			}
 
 			// -------------------------------------------
 			//  Category
 			// -------------------------------------------
 
-			if (isset($params['category']) && $params['category'])
+			if (!empty($params['category']))
 			{
-				// get filtered list of entry ids
-				$not = NULL;
-				$query = $this->EE->db->query('SELECT entry_id FROM exp_category_posts
-				                               WHERE cat_id '.$this->param2sql($params['category'], $not, FALSE).'
-				                               GROUP BY entry_id');
+				$join_cat_posts = TRUE;
 
-				if (! $query->num_rows())
+				$category = $this->_escape_variable($params['category']);
+
+				if (!is_array($category) && (strlen($category) > 4 && (strtolower(substr($category, 0, 4)) == 'not ') || (strtolower(substr($category, 0, 4)) == 'not_')))
 				{
-					if (! $not) return FALSE;
+					$null_condition = ' OR cp.cat_id IS NULL';
 				}
 				else
 				{
-					$entry_ids = array();
-					foreach ($query->result_array() as $row)
-					{
-						$entry_ids[] = $row['entry_id'];
-					}
-
-					$where[] = 'ct.entry_id '.$this->param2sql($entry_ids, $not);
+					$null_condition = '';
 				}
+
+				$where[] = '(cp.cat_id '.$this->param2sql($category) . $null_condition . ')';
 			}
 
 			// -------------------------------------------
 			//  Category Group
 			// -------------------------------------------
 
-			if (isset($params['category_group']) && $params['category_group'])
+			if (!empty($params['category_group']))
 			{
-				// get filtered list of entry ids
-				$not = NULL;
-				$query = $this->EE->db->query('SELECT cp.entry_id FROM exp_category_posts cp, exp_categories c
-				                               WHERE cp.cat_id = c.cat_id
-				                                     AND c.group_id '.$this->param2sql($params['category_group'], $not, FALSE).'
-				                               GROUP BY entry_id');
-
-				if (! $query->num_rows())
-				{
-					if (! $not) return FALSE;
-				}
-				else
-				{
-					$entry_ids = array();
-					foreach ($query->result_array() as $row)
-					{
-						$entry_ids[] = $row['entry_id'];
-					}
-
-					$where[] = 'ct.entry_id '.$this->param2sql($entry_ids, $not);
-				}
+				$join_channels = TRUE;
+				$where[] = 'c.cat_group '.$this->param2sql($this->_escape_variable($params['category_group']));
 			}
 
 			// -------------------------------------------
@@ -243,14 +217,19 @@ class Playa_Helper {
 
 			$timestamp = (isset($this->EE->TMPL) && $this->EE->TMPL->cache_timestamp) ? $this->EE->TMPL->cache_timestamp : $this->EE->localize->now;
 
+			if (! isset($params['show_expired']) || $params['show_expired'] != 'yes')
+			{
+				$where[] = '(ct.expiration_date = 0 OR ct.expiration_date > '.$timestamp.')';
+			}
+
 			if (! isset($params['show_future_entries']) || $params['show_future_entries'] != 'yes')
 			{
 				$where[] = 'ct.entry_date < '.$timestamp;
 			}
 
-			if (! isset($params['show_expired']) || $params['show_expired'] != 'yes')
+			if (isset($params['only_show_editable_entries']) && $params['only_show_editable_entries'] == 'yes')
 			{
-				$where[] = '(ct.expiration_date = 0 OR ct.expiration_date > '.$timestamp.')';
+				$where[] = 'ct.entry_date < '.$timestamp;
 			}
 
 			// -------------------------------------------
@@ -259,7 +238,7 @@ class Playa_Helper {
 
 			if (isset($params['entry_id']) && $params['entry_id'])
 			{
-				$where[] = 'ct.entry_id '.$this->param2sql($params['entry_id']);
+				$where[] = 'ct.entry_id '.$this->param2sql($this->_escape_variable($params['entry_id']));
 			}
 
 			// -------------------------------------------
@@ -268,7 +247,7 @@ class Playa_Helper {
 
 			if (isset($params['status']) && $params['status'])
 			{
-				$where[] = 'ct.status '.$this->param2sql($params['status']);
+				$where[] = 'ct.status '.$this->param2sql($this->_escape_variable($params['status']));
 			}
 
 			// -------------------------------------------
@@ -277,7 +256,7 @@ class Playa_Helper {
 
 			if (isset($params['url_title']) && $params['url_title'])
 			{
-				$where[] = 'ct.url_title '.$this->param2sql($params['url_title']);
+				$where[] = 'ct.url_title '.$this->param2sql($this->_escape_variable($params['url_title']));
 			}
 
 			// -------------------------------------------
@@ -286,34 +265,17 @@ class Playa_Helper {
 
 			if (! empty($params['site_id']))
 			{
-				$where[] = 'ct.site_id '.$this->param2sql($params['site_id']);
+				$where[] = 'ct.site_id '.$this->param2sql($this->_escape_variable($params['site_id']));
 			}
 
 			// -------------------------------------------
 			//  Channel
 			// -------------------------------------------
 
-			if (isset($params['channel']) && $params['channel'])
+			if (!empty($params['channel']))
 			{
-				// get channel IDs
-				$not = NULL;
-				$query = $this->EE->db->query('SELECT channel_id FROM exp_channels
-				                               WHERE channel_name '.$this->param2sql($params['channel'], $not, FALSE));
-
-				if (! $query->num_rows())
-				{
-					if (! $not) return FALSE;
-				}
-				else
-				{
-					$channel_ids = array();
-					foreach ($query->result_array() as $row)
-					{
-						$channel_ids[] = $row['channel_id'];
-					}
-
-					$where[] = 'ct.channel_id '.$this->param2sql($channel_ids, $not);
-				}
+				$join_channels = TRUE;
+				$where[] = 'c.channel_name '.$this->param2sql($this->_escape_variable($params['channel']));
 			}
 
 			// -------------------------------------------
@@ -322,7 +284,7 @@ class Playa_Helper {
 
 			if (isset($params['channel_id']) && $params['channel_id'])
 			{
-				$where[] = 'ct.channel_id '.$this->param2sql($params['channel_id']);
+				$where[] = 'ct.channel_id '.$this->param2sql($this->_escape_variable($params['channel_id']));
 			}
 
 			// -------------------------------------------
@@ -331,18 +293,50 @@ class Playa_Helper {
 
 			if (isset($params['keywords']) && $params['keywords'])
 			{
+				$params['keywords'] = $this->EE->db->escape_like_str($params['keywords']);
 				$where[] = 'ct.title LIKE "%'.$params['keywords'].'%"';
 			}
 
 		// -------------------------------------------
-		//  Add WHERE to SQL
+		//  Assemble the SQL
+		// -------------------------------------------
+
+		$sql = $sql_select;
+
+		// -------------------------------------------
+		//  Add FROM
+		// -------------------------------------------
+
+		$sql .= $sql_from;
+
+		// -------------------------------------------
+		//  Add JOINs
+		// -------------------------------------------
+
+		if ($join_cat_posts)
+		{
+			$sql .= "\nLEFT JOIN exp_category_posts cp ON cp.entry_id = ct.entry_id";
+		}
+
+		if ($join_members)
+		{
+			$sql .= "\nINNER JOIN exp_members m ON m.member_id = ct.author_id";
+		}
+
+		if ($join_channels)
+		{
+			$sql .= "\nINNER JOIN exp_channels c ON c.channel_id = ct.channel_id";
+		}
+
+		// -------------------------------------------
+		//  Add WHERE
 		// -------------------------------------------
 
 		if ($where || isset($add_to_sql['where']))
 		{
-			$sql .= ' WHERE '
-			      . implode(' AND ', $where)
-			      . (isset($add_to_sql['where']) ? ($where ? ' AND ' : '') . $add_to_sql['where'] : '');
+			$sql .= "\nWHERE "
+				  . implode("\nAND ", $where)
+				  . (isset($add_to_sql['where']) ? ($where ? "\nAND " : '') . $add_to_sql['where'] : '');
 		}
 
 		// -------------------------------------------
@@ -358,14 +352,15 @@ class Playa_Helper {
 			foreach ($orderbys as $i => $attr)
 			{
 				$sort = (isset($sorts[$i]) AND strtoupper($sorts[$i]) == 'DESC') ? 'DESC' : 'ASC';
-				$all_orderbys[] = 'ct.'.$attr.' '.$sort;
+				$attr = preg_replace('/[^a-z0-9\-_\.].*/', "", $attr);
+				$all_orderbys[] = 'ct.'.$this->_escape_variable($attr).' '.$sort;
 			}
 
-			$sql .=  ' ORDER BY '.implode(', ', $all_orderbys);
+			$sql .=  "\nORDER BY ".implode(', ', $all_orderbys);
 		}
 		else if (isset($add_to_sql['orderby']))
 		{
-			$sql .= ' ORDER BY '.$add_to_sql['orderby'];
+			$sql .= "\nORDER BY ".$add_to_sql['orderby'];
 
 			if (isset($params['sort']) && strtoupper($params['sort']) == 'DESC')
 			{
@@ -379,8 +374,8 @@ class Playa_Helper {
 
 		if ((isset($params['limit']) && $params['limit']) || (isset($params['offset']) && $params['offset']))
 		{
-			$offset = (isset($params['offset']) && $params['offset']) ? $params['offset'] . ', ' : '';
-			$limit  = (isset($params['limit']) && $params['limit']) ? $params['limit'] : 100;
+			$offset = (isset($params['offset']) && $params['offset']) ? (int) $params['offset'] . ', ' : '';
+			$limit  = (isset($params['limit']) && $params['limit']) ? (int) $params['limit'] : 100;
 
 			$sql .= ' LIMIT ' . $offset . $limit;
 		}
@@ -388,12 +383,33 @@ class Playa_Helper {
 		// -------------------------------------------
 		//  Run and return
 		// -------------------------------------------
-
 		$query = $this->EE->db->query($sql);
 
 		return isset($params['count']) ? $query->row('count') : $query->result();
 	}
 
+	/**
+	 * Escape a variable (array or plain) for SQL usage.
+	 *
+	 * @param $variable
+	 * @return array
+	 */
+	function _escape_variable($variable)
+	{
+		if (is_array($variable))
+		{
+			foreach ($variable as &$value)
+			{
+				$value = $this->EE->db->escape_str($value);
+			}
+		}
+		else
+		{
+			$variable = $this->EE->db->escape_str($variable);
+		}
+
+		return $variable;
+	}
 	// --------------------------------------------------------------------
 
 	/**
@@ -401,9 +417,31 @@ class Playa_Helper {
 	 */
 	function sort_entries(&$entries, $sort, $orderby)
 	{
-		usort($entries, create_function('$a, $b',
-			'return '.($sort == 'DESC' ? '-1 * ' : '').'strcmp($a->'.$orderby.', $b->'.$orderby.');'
-		));
+		self::$_order_by = $orderby;
+
+		usort($entries, array('Playa_Helper', 'entry_compare'));
+
+		if ($sort == 'DESC') {
+			$entries = array_reverse($entries);
+		}
+
+		self::$_order_by = NULL;
+	}
+
+	/**
+	 * Compare two entries.
+	 *
+	 * @param $a
+	 * @param $b
+	 * @return int
+	 */
+	public static function entry_compare($a, $b)
+	{
+		if (property_exists($a, self::$_order_by) && property_exists($b, self::$_order_by))
+		{
+			return 0;
+		}
+		return strcmp($a->{self::$_order_by}, $b->{self::$_order_by});
 	}
 
 	/**
@@ -429,7 +467,7 @@ class Playa_Helper {
 		if (! preg_match('/var_prefix=([\'"])(.+?)\1/', $params, $match))
 		{
 			$var_prefix = 'playa'.$this->EE->functions->random('alnum', 8);
-			$params .= ' var_prefix="'.$var_prefix.'"';	
+			$params .= ' var_prefix="'.$var_prefix.'"';
 
 			$tagdata = preg_replace('/(\{\/?)(?!(\/|exp\:))/', "$1{$var_prefix}:", $tagdata);
 		}
@@ -439,4 +477,21 @@ class Playa_Helper {
 		return LD.$tag.$params.RD.$tagdata.LD.'/'.$tag.RD;
 	}
 
+	/**
+	 * Get JSON formatted data for any given data.
+	 *
+	 * @param $data
+	 * @return string
+	 */
+	public function get_json($data)
+	{
+		if (version_compare(APP_VER, '2.6', '<') OR !function_exists('json_encode'))
+		{
+			return $this->EE->javascript->generate_json($data, TRUE);
+		}
+		else
+		{
+			return json_encode($data);
+		}
+	}
 }
--- system/expressionengine/third_party/playa/mcp.playa.php
+++ system/expressionengine/third_party/playa/mcp.playa.php
@@ -38,6 +38,8 @@ class Playa_mcp {
 	{
 		if (! isset($_POST['field_id']) || ! isset($_POST['field_name'])) exit('Invalid input data');
 
+		$_POST['field_name'] = preg_replace('/[^a-z0-9\-_\[\]]/i', '', $_POST['field_name']);
+
 		// -------------------------------------------
 		//  Main params
 		// -------------------------------------------
@@ -51,6 +53,7 @@ class Playa_mcp {
 		if (isset($_POST['channel']))  $params['channel_id'] = $this->EE->input->post('channel');
 		if (isset($_POST['category'])) $params['category']   = $this->EE->input->post('category');
 		if (isset($_POST['author']))   $params['author_id']  = $this->EE->input->post('author');
+		if (isset($_POST['member_groups']))   $params['member_groups']  = $this->EE->input->post('member_groups');
 		if (isset($_POST['status']))   $params['status']     = $this->EE->input->post('status');
 		if (isset($_POST['keywords'])) $params['keywords']   = $this->EE->input->post('keywords');
 
