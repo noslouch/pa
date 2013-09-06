d20927cd711cebcb88b93d8c5ea032a4dad53b4d
brian <me@brianwhitton.com>
2013-09-06 18:46:22 -0400

    started search form. made model for query. started overall search view file

---
+++ build/js/search.js
@@ -0,0 +1,16 @@
+/* views/search.js - Search view
+ *
+ * searchForm
+ * **********
+ * uses html in header include
+ * creates a new search query on show
+ * sets keyword on submit and calls model's search method
+ * advanced search dimensions are set/unset with checkboxes
+ * creates searchresults collection on success event
+ *
+ * searchResults
+ * *************
+ * navigated to on serachresults creation
+ * sorts/groups parses results (maybe make model for that)
+ * renders to page
+ */
