851958bc17cf2ec27b18998395e9bb7210ad4c29
Brian Whitton <me@brianwhitton.com>
2013-09-06 21:28:13 -0400

    started search form view. need to split up init from icon click

--- build/js/search.js
+++ build/js/search.js
@@ -4,7 +4,7 @@
  * **********
  * uses html in header include
  * creates a new search query on show
- * sets keyword on submit and calls model's search method
+ * sets keywords on submit and calls model's search method
  * advanced search dimensions are set/unset with checkboxes
  * creates searchresults collection on success event
  *
@@ -14,3 +14,39 @@
  * sorts/groups parses results (maybe make model for that)
  * renders to page
  */
+
+'use strict';
+var PA = PA || {}
+PA.dispatcher = PA.dispatcher || _.extend( {}, Backbone.Events )
+
+PA.SearchForm = Backbone.View.extend({
+    initialize : function() {
+        this.model = new PA.SearchQuery()
+        this.render()
+    },
+    render : function() {
+        this.keywords = this.$('#keywords')
+        this.$el.addClass('active')
+        this.$('#searchInput').focus()
+    },
+    events : {
+        'submit' : 'submit',
+        'click #cancelSearch' : 'cancel'
+    },
+    submit : function(e) {
+        e.preventDefault()
+        var keywords = this.keywords.val().trim()
+        this.model.set( 'keywords' , keywords )
+        this.model.search()
+        .done(function(d){
+            console.log(JSON.parse(d))
+        })
+    },
+    cancel : function(e) {
+        e.preventDefault()
+        this.keywords.val('')
+        this.$el.removeClass('active')
+        this.remove()
+    }
+})
+
