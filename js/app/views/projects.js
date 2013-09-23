6c9676fa9c3ad37d53252e00b250dca2ba14d297
Brian Whitton <me@brianwhitton.com>
2013-09-23 11:43:13 -0400

    testing domready as callback instead of plugin to avoid timeout error

--- js/app/views/projects.js
+++ js/app/views/projects.js
@@ -46,14 +46,16 @@ define([
             this.$el.html( pageView.render() )
             this.semantics( this.model.get('className'), this.model.get('outlineTitle') )
 
-            require(['app/views/showcaseviews'], function(S) {
+            require(['lib/requirejs/domReady','app/views/showcaseviews'], function(domReady, S) {
                 if ( pageModel.get('showcase') instanceof S.Image ) {
                     console.log('instanceof S.Image: loading isotope')
 
-                    pageModel.get('showcase').firstLoad()
-                    if ( !filtering ) {
-                        pageModel.set('filter', '*')
-                    }
+                    domReady(function() {
+                        pageModel.get('showcase').firstLoad()
+                        if ( !filtering ) {
+                            pageModel.set('filter', '*')
+                        }
+                    })
                 } else if ( pageModel.get('showcase') instanceof S.List ) {
                     console.log('instanceof S.List: sorting by name')
 
