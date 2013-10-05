de6bd8e3d14be1af5d8bf6898938d8f39393ae03
Brian Whitton <me@brianwhitton.com>
2013-10-05 14:26:14 -0400

    2.7.1 files

---
+++ core/installer/views/uptodate.php
@@ -0,0 +1,17 @@
+<h2><?=lang('running_current')?></h2>
+
+<div class="shade">
+
+<p class="important"><?=lang('delete_via_ftp')?></p>
+
+<p class="pad"><?=lang('folder_is_located_at')?> <em><?=$installer_path?></em></p>
+
+<p class="important"><strong><?=lang('no_access_until_delete')?></strong></p>
+
+</div>
+
+<h2><?=lang('bookmark_links')?></h2>
+
+<p><a href="<?=$cp_url?>" target="_blank"><?=lang('cp_located_here')?></a></p>
+
+<p><a href="<?=$site_url?>" target="_blank"><?=lang('site_located_here')?></a></p>
