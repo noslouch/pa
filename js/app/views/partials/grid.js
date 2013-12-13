aae9ff2c200ab4503cc6e46e12fa7c0a09c7d773
Brian Whitton <me@brianwhitton.com>
2013-12-12 21:06:20 -0500

    not using rows anymore in grids. working on mixitup filter controls

--- js/app/views/partials/grid.js
+++ js/app/views/partials/grid.js
@@ -6,12 +6,23 @@ define([
     'backbone',
     'underscore',
     'tpl/jst',
-    'imagesLoaded'
+    //'imagesLoaded',
+    'mixitup'
 ], function( $, Backbone, _, TPL ) {
 
     var GridThumb = Backbone.View.extend({
         tagName : 'div',
-        className : 'four-column-cell',
+        className : function() {
+            if ( this.model.has('tags') ) {
+                var tags = []
+                _.each( this.model.get('tags'), function(obj) {
+                    tags.push( obj.className )
+                }, this )
+                return 'photo-cell ' + tags.join(' ')
+            } else {
+                return 'photo-cell'
+            }
+        },
         template : TPL.gridThumb,
         render : function() {
             var html = this.template({
@@ -21,24 +32,30 @@ define([
                 summary : this.model.get('summary')
             })
             this.$el.append( html )
+            this.el.dataset.name = this.model.get('title')
+            this.el.dataset.year = this.model.get('date').year()
             return this.el
         }
     })
 
     var Grid = Backbone.View.extend({
         tagName : 'div',
-        className: 'four-column showcase',
+        className: 'photo-grid showcase',
         rowTmpl : TPL.gridRow,
         $row : undefined,
         initialize : function() {
             if ( this.$el.children().length ) { return this.el }
 
             this.collection.forEach( function(model, index){
-                if (index % 4 === 0) {
-                    this.$row = $( this.rowTmpl() ).addClass(this.class)
-                    this.$el.append(this.$row)
-                }
-                this.$row.append( new this.Thumb({
+                //if (index % 4 === 0) {
+                //    this.$row = $( this.rowTmpl() ).addClass(this.class)
+                //    this.$el.append(this.$row)
+                //}
+                //this.$row.append( new this.Thumb({
+                //    model : model
+                //}).render() )
+
+                this.$el.append( new this.Thumb({
                     model : model
                 }).render() )
             }, this )
@@ -47,9 +64,6 @@ define([
 
         render : function() {
             this.delegateEvents()
-            this.$('.four-column-row').imagesLoaded( function() {
-                $('.four-column-row').addClass('loaded')
-            })
             return this.el
         }
     })
@@ -65,17 +79,23 @@ define([
                         collection : collection
                     })
 
-                    Backbone.dispatcher.trigger(self.class + ':ready')
+                    Backbone.dispatcher.trigger(self.class + ':ready', self)
                 }
             })
+
+            $(window).on('hashchange', this.filter)
         },
 
         events : {
-            'click .four-column-cell a' : 'navigate'
+            'click .photo-cell a' : 'navigate'
         },
 
         render : function() {
             this.$el.html( this.grid.render() )
+            this.$('.showcase').mixitup({
+                targetSelector : '.photo-cell',
+                filterSelector : ''
+            })
         },
 
         onClose : function() {
@@ -99,6 +119,9 @@ define([
         navigate : function(e) {
             e.preventDefault()
             Backbone.dispatcher.trigger('navigate:detail', e, this)
+        },
+
+        filter : function(e) {
         }
     })
 
